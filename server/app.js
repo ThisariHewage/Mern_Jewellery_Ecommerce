126810fe7f39aeef15111cfb172b6cb6dabc49ca
Thisari428-bit <ureshathisari@gmail.com>
2026-06-13 00:20:19 +0530

    explicitly allow Authorization header in CORS configuration

--- server/app.js
+++ server/app.js
@@ -33,10 +33,13 @@ const corsOptions = {
         if (allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
+            console.log(`[CORS] Blocked Origin: ${origin}`);
             callback(new Error("Not allowed by CORS"));
         }
     },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
     optionsSuccessStatus: 200,
 };
 
