972348fb21ebc734d959eeec6faabf3d72fd7bf5
Thisari428-bit <ureshathisari@gmail.com>
2026-06-13 00:26:23 +0530

    add PATCH to allowed CORS methods

--- server/app.js
+++ server/app.js
@@ -38,7 +38,7 @@ const corsOptions = {
         }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
     optionsSuccessStatus: 200,
 };
